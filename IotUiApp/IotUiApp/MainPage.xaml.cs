561939670ca16f194e3d921467ef0b918349d017
ancab <anca_denisa.barbu@cti.pub.ro>
2016-03-25 15:43:37 +0200

    Speech2Text functonality integrated - tested with simulated device

--- IotUiApp/IotUiApp/MainPage.xaml.cs
+++ IotUiApp/IotUiApp/MainPage.xaml.cs
@@ -31,44 +31,12 @@ namespace IotUiApp
 
     public sealed partial class MainPage : Page
     {
+        private static string audioTempCommand;
 
         public MainPage()
         {
             this.InitializeComponent();
             MyWebView.ScriptNotify += webView_ScriptNotify;
-        }
-
-        async void webView_ScriptNotify(object sender, NotifyEventArgs e)
-        {
-            string value = e.Value;
-            if (value.Contains("temperature="))
-            {
-                var tempValueCommand = value;
-
-                BackgroundWorker bw = new BackgroundWorker();
-
-                // send command and process it in background
-                bw.DoWork += new DoWorkEventHandler(
-                delegate (object o, DoWorkEventArgs args)
-                {
-                    SendCloudToDevice.sendCommand(tempValueCommand);
-
-                });
-
-                //when worker completes its task notify user inerface by invoking a javascript 
-                //method that updates command timestamp in the ui
-                bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
-                delegate (object o, RunWorkerCompletedEventArgs args)
-                {
-                    CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        MyWebView.InvokeScriptAsync("updateCommandTimestamp", new string[] {DateTime.Now.ToString("HH:mm:ss") });
-                    });
-                });
-
-                bw.RunWorkerAsync();
-            }
-
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -77,46 +45,124 @@ namespace IotUiApp
             MyWebView.Navigate(u);
         }
 
-        private void getMessageFromPartition(string partition)
-        {
-            string message = ReadDeviceToCloud.getMessage(partition);
-            ReceivedData receivedData;
-            if (message != null)
-
-            {
-                receivedData = JsonConvert.DeserializeObject<ReceivedData>(message);
-                CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    MyWebView.InvokeScriptAsync("updateValues", new string[] { receivedData.temperature.ToString(), receivedData.humidity.ToString(), DateTime.Now.ToString("HH:mm:ss") });
-                });
-                Debug.WriteLine("{0}  {1}  {2}", receivedData.heater, receivedData.temperature, receivedData.humidity);
-            }
-        }
 
 
         void LoadCompleted(object sender, NavigationEventArgs e)
         {
             BackgroundWorker bw = new BackgroundWorker();
-
-
-            // process received messages from both parititons in background 
-            bw.DoWork += new DoWorkEventHandler(
-            delegate (object o, DoWorkEventArgs args)
-            {
-                getMessageFromPartition("1");
-
-            });
-
-            bw.DoWork += new DoWorkEventHandler(
-            delegate (object o, DoWorkEventArgs args)
-            {
-                getMessageFromPartition("0");
-
-            });
-
-            bw.RunWorkerAsync();
+            RunBackgroundTaskWithParam("1", UpdateValuesFromPartitionTask, DoesNothing);
+            RunBackgroundTaskWithParam("2", UpdateValuesFromPartitionTask, DoesNothing);
+            RunBackgroundTask(CheckAudioTemperatureCommandTask);
 
         }
 
+        async void webView_ScriptNotify(object sender, NotifyEventArgs e)
+        {
+            string value = e.Value;
+            if (value.Contains("temperature="))
+            {
+                var tempValueCommand = value;
+                SendCommandMechanism(tempValueCommand);
+            }else if(value == "enable_speech")
+            {
+                SpeechRecognitionEngine.startSpeechRecognitionMechanism();
+            }
+
+        }
+        public void SendCommandMechanism(string tempValueCommand)
+        {
+            RunBackgroundTaskWithParam(tempValueCommand, SendCloudToDevice.sendCommand, UpdateCommandTimestampTask);
+
+        }
+
+
+        public void InvokeJSScript(string scriptName, string[] parameters)
+        {
+            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                MyWebView.InvokeScriptAsync(scriptName, parameters);
+            });
+
+        }
+
+        public delegate void MyTask();
+        public delegate void MyTaskWithParam(string param);
+
+        public void RunBackgroundTask(MyTask task)
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+
+            // send task and process it in background
+            bw.DoWork += new DoWorkEventHandler(
+            delegate (object o, DoWorkEventArgs args)
+            {
+                task();
+
+            });
+            bw.RunWorkerAsync();
+        }
+
+        public void RunBackgroundTaskWithParam(string param, MyTaskWithParam task, MyTask completed)
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+
+            // send task and process it in background
+            bw.DoWork += new DoWorkEventHandler(
+            delegate (object o, DoWorkEventArgs args)
+            {
+                task(param);
+
+            });
+            //when worker completes its task notify user inerface by invoking a javascript 
+            //method that updates command timestamp in the ui
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+            delegate (object o, RunWorkerCompletedEventArgs args)
+            {
+                completed();
+            });
+            bw.RunWorkerAsync();
+        }
+
+
+
+
+
+        public void DoesNothing()
+        {
+
+        }
+
+        public void CheckAudioTemperatureCommandTask()
+        {
+            while (true)
+            {
+                if (audioTempCommand != null)
+                {
+                    SendCommandMechanism(audioTempCommand);
+                    audioTempCommand = null;
+                }
+            }
+        }
+        public void UpdateCommandTimestampTask()
+        {
+            InvokeJSScript("updateCommandTimestamp", new string[] { DateTime.Now.ToString("HH:mm:ss") });
+        }
+
+        public void UpdateValuesFromPartitionTask(string partition)
+        {
+            string message = ReadDeviceToCloud.getMessage(partition);
+            ReceivedData receivedData;
+            if (message != null)
+            {
+                receivedData = JsonConvert.DeserializeObject<ReceivedData>(message);
+                InvokeJSScript("updateValues", new string[] { receivedData.temperature.ToString(), receivedData.humidity.ToString(), DateTime.Now.ToString("HH:mm:ss") });
+                Debug.WriteLine("{0}  {1}  {2}", receivedData.heater, receivedData.temperature, receivedData.humidity);
+            }
+        }
+
+        public static void setAudioTempCommand(string command)
+        {
+            audioTempCommand = command;
+        }
     }
 }
---
+++ IotUiApp/IotUiApp/SpeechRecognitionEngine.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+using Windows.Media.SpeechRecognition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.SpeechSynthesis;
+using System.Diagnostics;
+
+namespace IotUiApp
+{
+    class SpeechRecognitionEngine
+    {
+        private static SpeechRecognizer m_recognizer;
+        // Keep track of whether the recognizer is currently listening for user input
+        private static bool m_isListening = false;
+        private static int s_continuousRecognitionAutoStopSilenceTimeout = 3600;
+        private static uint s_maxRecognitionResultAlternates = 3;
+        private static async void init()
+        {
+            m_recognizer = new SpeechRecognizer();
+            SpeechRecognitionTopicConstraint topicConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "Development");
+            m_recognizer.Constraints.Add(topicConstraint);
+            SpeechRecognitionCompilationResult result = await m_recognizer.CompileConstraintsAsync();   // Required
+
+            if (result.Status != SpeechRecognitionResultStatus.Success)
+            {
+                Debug.WriteLine("Grammar Compilation Failed: " + result.Status.ToString());
+
+            }
+
+            m_recognizer.ContinuousRecognitionSession.AutoStopSilenceTimeout = System.TimeSpan.FromSeconds(s_continuousRecognitionAutoStopSilenceTimeout);
+            m_recognizer.ContinuousRecognitionSession.Completed += OnContinuousRecognitionSessionCompletedHandler;
+            m_recognizer.ContinuousRecognitionSession.ResultGenerated += OnContinuousRecognitionSessionResultGeneratedHandler;
+        }
+
+        /// <summary>
+        /// Handle events fired when error conditions occur, such as the microphone becoming unavailable, or if
+        /// some transient issues occur.
+        /// </summary>
+        /// <param name="sender">The continuous recognition session</param>
+        /// <param name="args">The state of the recognizer</param>
+        private static async void OnContinuousRecognitionSessionCompletedHandler(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            if (args.Status != SpeechRecognitionResultStatus.Success)
+            {
+                m_isListening = false;
+                //TO DO ANCA - notifica faptul ca s a terminat inegistrarea
+
+            }
+        }
+        /// <summary>
+        /// Handle events fired when a result is generated in the continuous recognition mode.
+        /// </summary>
+        /// <param name="sender">The Recognition session that generated this result</param>
+        /// <param name="args">Details about the recognized speech</param>
+        private static void OnContinuousRecognitionSessionResultGeneratedHandler(SpeechContinuousRecognitionSession sender, SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            SpeechRecognitionResult result = args.Result;
+            if (result.Confidence != SpeechRecognitionConfidence.Rejected)
+            {
+                var altResults = result.GetAlternates(s_maxRecognitionResultAlternates);
+                uint idx = 0;
+                foreach (var curentAltResult in altResults)
+                {
+                    if (curentAltResult.Confidence == SpeechRecognitionConfidence.Rejected)
+                    {
+                        break;
+                    }
+                    //TO DO ANCA - verifica notifica daca nu e numar valid
+
+                    MainPage.setAudioTempCommand("temperature=" + curentAltResult.Text.Remove(curentAltResult.Text.Length - 1) + "\0");
+                    idx++;
+                }
+            }
+            else
+            {
+                //TO DO ANCA - notifica faptul ca nu s-a inteles
+            }
+
+        }
+
+        private static async void starListening()
+        {
+            var recognition = new Object();
+
+            if (m_isListening == false)
+            {
+
+                m_isListening = true;
+
+                try
+                {
+                    await m_recognizer.ContinuousRecognitionSession.StartAsync();
+                }
+                catch (Exception ex)
+                {
+                    const int privacyPolicyHResult = unchecked((int)0x80045509);
+                    const int networkNotAvailable = unchecked((int)0x80045504);
+
+                    if (ex.HResult == privacyPolicyHResult)
+                    {
+                        // User has not accepted the speech privacy policy
+                        //this.PromptForSpeechRecognitionPermission();
+                        //new MessageDialog("You will need to accept the speech privacy policy in order to use speech recognition in this app.").ShowAsync();
+                    }
+                    else if (ex.HResult == networkNotAvailable)
+                    {
+                        Debug.WriteLine("bau");
+                    }
+                    else
+                    {
+                        //this.Dispatcher.BeginInvoke(delegate { MessageBox.Show(ex.Message); });
+                    }
+                }
+            }
+
+        }
+
+        public static void startSpeechRecognitionMechanism()
+        {
+            init();
+            starListening();
+        }
+    }
+}
